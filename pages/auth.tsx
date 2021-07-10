7725e8a4ee1b830573b54e4ea800a882a2009736
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-07-10 18:10:52 +0530

    fix: auth redirect

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useEffect } from 'react'
 import FirebaseAuth from '../firebase/firebaseAuth'
 import firebase from './../firebase/clientApp'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useRouter } from 'next/dist/client/router'
+
 
 const auth: React.FC = () => {
-
   const uiConfig = {
     signInFlow: 'popup',
     signInSuccessUrl: '/',
     signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID]
   }
+  const router = useRouter()
+  const [user, userLoading,] = useAuthState(firebase.auth())
+  useEffect(() => {
+    if (!userLoading && user) router.push('/')
+  }, [user, userLoading, router])
 
   return (
     <div className='wrapper auth'>
